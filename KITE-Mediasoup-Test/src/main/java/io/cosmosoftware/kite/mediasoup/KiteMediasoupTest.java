b6f6e619745a2009abc59a4af881772487a38089
LimZhiEn <46666223+LimZhiEn@users.noreply.github.com>
2019-05-13 15:06:27 +0800

    removed userId because it is not necessary, mediasoup automatically assigns a unique upon joining the room

--- KITE-Mediasoup-Test/src/main/java/io/cosmosoftware/kite/mediasoup/KiteMediasoupTest.java
+++ KITE-Mediasoup-Test/src/main/java/io/cosmosoftware/kite/mediasoup/KiteMediasoupTest.java
@@ -6,23 +6,19 @@ import io.cosmosoftware.kite.mediasoup.steps.GetStatsStep;
 import io.cosmosoftware.kite.mediasoup.steps.JoinVideoCallStep;
 import io.cosmosoftware.kite.mediasoup.steps.ScreenshotStep;
 import io.cosmosoftware.kite.mediasoup.steps.StayInMeetingStep;
+import org.openqa.selenium.WebDriver;
 import org.webrtc.kite.tests.KiteBaseTest;
 import org.webrtc.kite.tests.TestRunner;
-import io.cosmosoftware.kite.util.TestUtils;
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
 
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 
 import static org.webrtc.kite.Utils.getStackTrace;
 
-
 public class KiteMediasoupTest extends KiteBaseTest {
 
   private int loadReachTime = 0;
 
-  
   @Override
   protected void payloadHandling() {
     super.payloadHandling();
@@ -30,7 +26,7 @@ public class KiteMediasoupTest extends KiteBaseTest {
     String[] rooms = null;
     if (jsonPayload != null) {
       loadReachTime = jsonPayload.getInt("loadReachTime", loadReachTime);
-      setExpectedTestDuration(Math.max(getExpectedTestDuration(), (loadReachTime + 300)/60));
+      setExpectedTestDuration(Math.max(getExpectedTestDuration(), (loadReachTime + 300) / 60));
       JsonArray jsonArray = jsonPayload.getJsonArray("rooms");
       rooms = new String[jsonArray.size()];
       for (int i = 0; i < jsonArray.size(); i++) {
@@ -41,20 +37,24 @@ public class KiteMediasoupTest extends KiteBaseTest {
       getRoomManager().setRoomNames(rooms);
     }
   }
-  
+
   @Override
   public void populateTestSteps(TestRunner runner) {
     try {
       WebDriver webDriver = runner.getWebDriver();
-      int id = runner.getId();
-      String roomUrl = getRoomManager().getRoomUrl()  + "&username=user" + TestUtils.idToString(id);
+      String roomUrl = getRoomManager().getRoomUrl();
       runner.addStep(new JoinVideoCallStep(webDriver, roomUrl));
       if (!this.fastRampUp()) {
         runner.addStep(new FirstVideoCheck(webDriver));
         runner.addStep(new AllVideoCheck(webDriver, getMaxUsersPerRoom()));
         if (this.getStats()) {
-          runner.addStep(new GetStatsStep(webDriver, getMaxUsersPerRoom(), getStatsCollectionTime(),
-                  getStatsCollectionInterval(), getSelectedStats()));
+          runner.addStep(
+              new GetStatsStep(
+                  webDriver,
+                  getMaxUsersPerRoom(),
+                  getStatsCollectionTime(),
+                  getStatsCollectionInterval(),
+                  getSelectedStats()));
         }
         if (this.takeScreenshotForEachTest()) {
           runner.addStep(new ScreenshotStep(webDriver));
